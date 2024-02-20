03a5f615ab0e27c724683486d708d18a38e386ac
David McFadzean <davidmc@gmail.com>
2024-02-20 14:34:06 -0500

    Added helia

--- server.js
+++ server.js
@@ -1,17 +1,50 @@
-// import { createHelia } from 'helia';
-// import { FsBlockstore } from 'blockstore-fs';
-// import { json } from '@helia/json';
+import { createHelia } from 'helia';
+import { FsBlockstore } from 'blockstore-fs';
+import { json } from '@helia/json';
 
 import axios from 'axios';
+import cron from 'node-cron';
+
+const blockstore = new FsBlockstore('./ipfs');
+const helia = await createHelia({ blockstore });
+let ipfs = json(helia);
 
 async function getJoke() {
     const response = await axios.get('https://icanhazdadjoke.com/', {
         headers: {
-            Accept: 'application/json',
-        }});
+            "User-Agent": 'joker (https://github.com/macterra/joker)',
+            "Accept": 'application/json',
+        }
+    });
 
-    console.log(response.data);
     return response.data;
 }
 
-getJoke();
+async function publishJoke(joke) {
+    const cid = await ipfs.add(joke);
+    return cid;
+}
+
+async function logJoke(cid) {
+    const joke = await ipfs.get(cid);
+    console.log(cid);
+    console.log(joke.joke);
+    console.log('---');
+}
+
+async function main() {
+    const joke = await getJoke();
+    const cid = await publishJoke(joke);
+    logJoke(cid);
+}
+
+cron.schedule('* * * * *', async () => {
+    try {
+        await main();
+    }
+    catch (error) {
+        console.error(`Error in cron job: ${error}`);
+    }
+});
+
+main();
